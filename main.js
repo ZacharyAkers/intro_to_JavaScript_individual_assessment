a3aa233dff00a5be6bcb9d1eba21f67f6271868f
ZacharyAkers <user@Users-MacBook-Air.local>
2017-06-08 20:18:17 -0400

    resubmit

--- main.js
+++ main.js
@@ -1,224 +1,85 @@
-// Answer the following questions in this file using comments without running the code
-
 // 1.
-// What does `givenName` equal right now?
-var givenName;
-// A:undefined
-
-
+// Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
+// A:
+function max(num1, num2){
+    // Your answer here
+if (num1 > num2) {
+  return num1;}
+  else if (num2 > num1){
+  return num2;}
+}
 // 2.
-// What is `givenName` set to right now?
-givenName = "Tim";
-// A:Tim
-
-
+// Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
+// A:
+function maxOfThree(num1, num2, num3){
+    // Your answer here
+if (num1 > num2 || num1 > num3){
+  return num1;}
+  else if (num2 > num1 || num2 > num3) {
+  return num2;}
+  else if (num3 > num1 || num3 > num2) {
+  return num3;
+  }
+}
 // 3.
-// What is `givenName` set to right now?
-givenName = givenName;
-// A:Tim
-
-
+// Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
+// A:
+function isVowel(string1){
+    // Your answer here
+if (string1 === a || string1 === e || string1 === i|| string1 === o || string1 === u){
+  return true;
+}
+else {return false;}
+}
 // 4.
-// What is `greeting` set to?
-var greeting = "Hello, how are you? " + givenName;
-// A:Hello, how are you? Tim
-
-
+// Write a function called `sum` that takes two parameters and returns the sum of those 2 numbers.
+// A:
+ function sum (num1, num2) {
+  return num1 + num2;
+}
 // 5.
-// What is `math` set to?
-var high = 50;
-var low  = 10;
-var math = high - low;
-// A:40
-
-
+// Write a function named `avg` that takes 3 parameters and returns the average of those 3 numbers.
+// A:
+function avg(num1, num2, num3) {
+  return (num1 + num2 + num3)/(3);
+}
 // 6.
-// What is `math` set to?
-math = high - "5";
-// A:45
-
-
+// Write a function called `getLength` that takes one parameter (a string) and returns the length
+// A:
+function getLength (value){
+  return value.length;
+}
 // 7.
-// Create a variable to calculate Tim's age
-// The answer should read "Tim is 33 years old"
-// The answer shoud not be written in a comment.
-var born = 1984;
-var today = 2017;
-// A:var age = "tim is" +(today - born)+ "years old"
-
-
+// Write a function called `greaterThan` that takes two parameters
+// and returns `true` if the second parameter is greater than the first.
+// Otherwise the function should return `false`.
+// A:
+var evaluateGreaterThan = function(num1, num2) {
+  if (num1 < num2)
+  return true;
+  else{
+    return false;
+  }
+}
 // 8.
-// Adjust this code. Store some information in the following variables.
-// A:yourName = "Zachary". instructorName = "Issac".
-var yourName;
-var instructorName;
-
-
+// Write a function called `greet` that takes a
+// single parameter and returns a string that
+// is formated like "Hello, Name!" where *Name*
+// is the parameter that was passed in.
+// A:
+function greet(name) {
+  return "Hello "+ name
+}
+console.log(greet("Grant"));
 // 9.
-// Update the variables `yourName` and `instructorName` so the expression below reads correctly.
-// A:Zachary is taking a class at The Iron Yard, my instructors's name is Issac. "Zachary"+"is taking a class at The Iron Yard, my instructor's name is"+"Issac"
-
-
-// This statement should read correctly
-var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
-
-
-// 10.
-// We've created a variable named after each primitive type.
-// Use the `=` operator to assign a value to each variable matching the type in the name.
-var myNumber;
-var myString;
-var myBoolean;
-// This one's a little tricky - think carefully about what makes a value "undefined"!
-var myUndefined;
-// A:myNumber is 5, myString is "This is a string", myBoolean is false
-
-
-// For questions 11 - 19: List which boolean value will be stored in each variable. (Remember: `==` does not check data type)
-
-// 11.
-var x = (false == 0);
-// A. false
-
-
-// 12.
-var y = (false == "");
-// A. false
-
-
-// 13.
-var z = (0 == "");
-// A.false
-
-
-// 14.
-var a = (null == null);
-// A.false
-
-
-// 25.
-var b = (undefined == undefined);
-// A.false
-
-
-// 16.
-var c = (undefined == null);
-// A.false
-
-
-// 17.
-var d = (null == false);
-// A.false
-
-
-// 18.
-var e = (NaN == null);
-// A.false
-
-
-// 19.
-var f = (NaN == NaN);
-// A.false
-
-
-// 20.
-// Fix the `if...else` statement below by adding an `if` keyword and conditional expression.
-// Use the variable `thirsty` as part of your conditional expression.
-// A:if (thirsty){}
-var thirsty = true;
-
-{
-  console.log("I'm parched!");
-} else {
-  console.log("I've had enough to drink.");
+// Write a function called `madlib` that takes 4 or more parameters (words).
+// The function should insert the words into a pre-defined sentence.
+// Finally the function should return that sentence.
+// Note: When I say words and sentence I mean strings. For example:
+// words: "quick", "fox", "fence"
+// sentence: "quick brown fox jumps over the fence"
+// A:
+function madlib(word1, word2, word3, word4, word5){
+  return "The "+ word1 + " added "+ word2 + " our "+ word3 +" help "+ word4 + " these "+ word5 +" ."
 }
-
-// For questions 21 - 29 list which statement will be logged.
-
-// 21.
-var x;
-
-if(x){
-	console.log("True!");
-} else {
-	console.log("False!");
-}
-// A:true
-
-
-// 22.
-if(""){
-	console.log("True!");
-} else {
-	console.log("False!");
-}
-// A:false
-
-
-// 23.
-// this expression will set x to NaN
-var x = 1 * undefined;
-
-if(x){
-	console.log("True!");
-} else {
-	console.log("False!");
-}
-// A:false
-
-
-// 24.
-if(false){
-	console.log("True!");
-} else {
-	console.log("False!");
-}
-// A:false
-
-
-// 25.
-if(-5 && 5){
-	console.log("True!");
-} else {
-	console.log("False!");
-}
-// A:true
-
-
-// 26.
-if("Hello World!"){
-	console.log("True!");
-} else {
-	console.log("False!");
-}
-// A:true
-
-
-// 27.
-if({}){
-	console.log("True!");
-} else {
-	console.log("False!");
-}
-// A:true
-
-
-// 28.
-if({name: "Yehuda Katz"}){
-	console.log("True!");
-} else {
-	console.log("False!");
-}
-// A:true
-
-// 29.
-if(new Date()){
-	console.log("True!");
-} else {
-	console.log("False!");
-}
-// A:true
-
-
-// 30.
-// Link this file to the sibling `index.html` document, open the file in your browser and request an instructor.
+console.log(madlib("words", "to","lesson", "solve", "functions"));
